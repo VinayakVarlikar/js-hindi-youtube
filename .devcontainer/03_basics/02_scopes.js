387b669c7127e78c5616dcdbca25fe63a39c457f
VinayakVarlikar <101932412+VinayakVarlikar@users.noreply.github.com>
2024-11-18 14:12:13 +0000

    scopes

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -4,9 +4,40 @@ if (true) {
   let a = 10;
   const b = 20;
   //   var c = 30;
-  console.log("Inner:", a);
+  // console.log("Inner:", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+  const username = "vinayak";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+
+  // two();
+}
+// one();
+if (true) {
+  const username = "vinayak";
+  if (username === "vinayak") {
+    const website = " youtube";
+    // console.log(username + website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++ interesting +++++++++++++++++++++++++
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+addTwo(5);
+const addTwo = function (num) {
+  return num + 2;
+};
