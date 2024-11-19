54524bd9a7163bfa198bf1479c0716b8a9e7cc18
VinayakVarlikar <101932412+VinayakVarlikar@users.noreply.github.com>
2024-11-19 17:13:09 +0000

    reduce

--- .devcontainer/05_iterations/seven.js
+++ .devcontainer/05_iterations/seven.js
@@ -4,12 +4,14 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // });
 // const newNums = myNums.map((num) => num + 10);
 
-
 // const newNum = myNums.forEach((nums) => {
 //   console.log(nums + 10);
 //   return nums;
 // });
 
+const newNumber = myNums
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num >= 40);
 
-
-console.log(newNums);
+console.log(newNumber);
