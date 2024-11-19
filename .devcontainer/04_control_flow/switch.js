061fd49edc599c9ea1703014cafdc730b74569df
VinayakVarlikar <101932412+VinayakVarlikar@users.noreply.github.com>
2024-11-19 02:35:11 +0000

    control flow

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }  basic syntax
+
+const month = "Mar";
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "Mar":
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("April");
+    break;
+
+  default:
+    console.log("default caase match");
+    break;
+}
